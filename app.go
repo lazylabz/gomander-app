97da3f01aa3d2db5c9992e151407365604bedfbb
moisesrj97 <moisesrj97@gmail.com>
2025-07-16 19:30:45 +0200

    wip: send logs test

--- app.go
+++ app.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"io"
+	"os/exec"
+	"strings"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -18,4 +24,57 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *App) ExecCommand(arg string) error {
+	chunks := strings.Split(arg, " ")
+	command := chunks[0]
+	args := chunks[1:]
+
+	cmd := exec.Command(command, args...)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		runtime.LogError(a.ctx, err.Error())
+		return err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		runtime.LogError(a.ctx, err.Error())
+		return err
+	}
+
+	if err := cmd.Start(); err != nil {
+		runtime.LogError(a.ctx, err.Error())
+		return err
+	}
+
+	// Stream stdout
+	go a.streamOutput(stdout, "stdout")
+	// Stream stderr
+	go a.streamOutput(stderr, "stderr")
+
+	// Optional: Wait in background
+	go func() {
+		err := cmd.Wait()
+		if err != nil {
+			runtime.LogError(a.ctx, err.Error())
+			return
+		}
+		runtime.EventsEmit(a.ctx, "processFinished", nil)
+	}()
+
+	return nil
+}
+
+func (a *App) streamOutput(pipeReader io.ReadCloser, streamType string) {
+	scanner := bufio.NewScanner(pipeReader)
+	for scanner.Scan() {
+		line := scanner.Text()
+		runtime.EventsEmit(a.ctx, "processLog", map[string]string{
+			"type": streamType,
+			"line": line,
+		})
+	}
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css'
+import {Children, useState} from "react";
+import {EventsOn} from "../wailsjs/runtime";
+import {ExecCommand} from "../wailsjs/go/main/App";
 
 function App() {
-  return (
-    <>
-      <button>Click me</button>
-    </>
-  )
+    const [logs, setLogs] = useState<{type: string, line: string}[]>([])
+    const [inputText, setInputText] = useState("")
+
+    EventsOn("processLog", (event: {type: string, line: string}) => {
+        setLogs((prev) => [...prev, event])
+    })
+
+    return (
+        <div className="app-container">
+            <input value={inputText} onChange={e => setInputText(e.target.value)} />
+            <button onClick={() => ExecCommand(inputText || "echo POTATO")}>Click me</button>
+            <div>
+                {Children.toArray(logs.map((line) => <p>[{line.type}]: {line.line}</p>))}
+            </div>
+        </div>
+    )
 }
 
 export default App
