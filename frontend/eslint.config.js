afb231c960b0f06a199e3ab4c8ef67cb080fa608
Moises Rodriguez <moisesrj97@gmail.com>
2025-07-17 19:27:20 +0200

    feat: shadcn basic setup

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,4 +20,11 @@ export default tseslint.config([
             globals: globals.browser,
         },
     },
+    // Shadcn UI components
+    {
+        files: ["**/components/ui/**/*.{ts,tsx}"],
+        rules: {
+            "react-refresh/only-export-components": "off",
+        }
+    }
 ])
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,28 @@ import {GetCommands} from "../wailsjs/go/main/LogServer";
 import type {Command} from "./types/contracts.ts";
 import {Event} from "./types/contracts.ts";
 
+import {Button} from "@/components/ui/button.tsx";
+import {
+    Dialog, DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog.tsx";
+
 function App() {
-    const [commands, setCommands] = useState<Record<string, Command>>({})
+    const [, setCommands] = useState<Record<string, Command>>({});
+    const [createModalOpen, setCreateModalOpen] = useState(false);
 
     const refreshCommands = async () => {
         const commandsData = await GetCommands();
 
         setCommands(commandsData);
+    }
+
+    const openCreateModal = () => {
+        setCreateModalOpen(true);
     }
 
     useEffect(() => {
@@ -20,9 +35,28 @@ function App() {
     })
 
     return (
-        <div className="w-full h-full bg-white">
-            Hello World! {JSON.stringify(commands)}
-        </div>
+        <main className="w-full h-full bg-white">
+            <Dialog open={createModalOpen} onOpenChange={setCreateModalOpen}>
+                <form>
+                    <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                            <DialogTitle>Add new command</DialogTitle>
+                            <DialogDescription>
+                                Make changes to your profile here.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                            <DialogClose asChild>
+                                <Button variant="outline">Cancel</Button>
+                            </DialogClose>
+                            <Button type="submit">Create</Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </form>
+            </Dialog>
+            <Button onClick={openCreateModal} variant="outline" className="fixed top-2 right-2 cursor-pointer">Add new
+                command</Button>
+        </main>
     )
 }
 
