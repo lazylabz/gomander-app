d8442a666dc87778da903a1af02d52ff7171273a
moisesrj97 <moisesrj97@gmail.com>
2025-07-22 18:39:42 +0200

    fix: avoid horizontal overflow in logs

--- frontend/src/screens/LogsScreen.tsx
+++ frontend/src/screens/LogsScreen.tsx
@@ -15,13 +15,14 @@ export const LogsScreen = () => {
   const { currentLogs, clearCurrentLogs } = useDataContext();
 
   return (
-    <div className="p-4 overflow-y-auto h-full">
+    <div className="p-4 overflow-y-auto h-full w-full">
       <BrushCleaning
         onClick={clearCurrentLogs}
         className="fixed top-3 right-6 z-1 text-foreground opacity-25 hover:opacity-100 transition-opacity cursor-pointer"
       />
       {currentLogs.map((log, index) => (
-        <div
+        <p
+          className="w-full wrap-anywhere"
           dangerouslySetInnerHTML={{ __html: ansiConverter.toHtml(log) }}
           key={index}
         />
