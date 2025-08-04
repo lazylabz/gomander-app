cb7deefec8db63321200534e8089c362c089db43
Noel Lopez <nlopez.publi@gmail.com>
2025-08-04 17:48:05 +0200

    feat: remove recent working directories from field

--- cmd/gomander/frontend/src/components/modals/Command/common/CommandWorkingDirectoryField.tsx
+++ cmd/gomander/frontend/src/components/modals/Command/common/CommandWorkingDirectoryField.tsx
@@ -1,17 +1,6 @@
-import { PopoverTrigger } from "@radix-ui/react-popover";
-import { FolderCode } from "lucide-react";
-import { useMemo, useState } from "react";
 import { useFormContext } from "react-hook-form";
 
 import type { FormSchemaType } from "@/components/modals/Command/common/formSchema.ts";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command.tsx";
 import {
   FormControl,
   FormField,
@@ -20,37 +9,9 @@ import {
   FormMessage,
 } from "@/components/ui/form.tsx";
 import { Input } from "@/components/ui/input.tsx";
-import { Popover, PopoverContent } from "@/components/ui/popover";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { useCommandStore } from "@/store/commandStore.ts";
 
 export const CommandWorkingDirectoryField = () => {
   const form = useFormContext<FormSchemaType>();
-  const commands = useCommandStore((state) => state.commands);
-
-  const [open, setOpen] = useState(false);
-
-  const workingDirSuggestions = useMemo(
-    () =>
-      Array.from(
-        new Set(
-          Object.values(commands)
-            .map((wd) => wd.workingDirectory.trim())
-            .filter(Boolean),
-        ),
-      ).map((workDir) => ({
-        id:
-          Object.values(commands).find(
-            (cmd) => cmd.workingDirectory.trim() === workDir,
-          )?.id || "",
-        workDir,
-      })),
-    [commands],
-  );
 
   return (
     <FormField
@@ -60,51 +21,7 @@ export const CommandWorkingDirectoryField = () => {
         <FormItem>
           <FormLabel>Working Directory</FormLabel>
           <FormControl>
-            <div className="w-full flex items-center gap-2">
-              <Input className="flex-1" {...field} />
-              <Popover open={open} onOpenChange={setOpen}>
-                <PopoverTrigger>
-                  <Tooltip>
-                    <TooltipTrigger type="button" className="flex items-center">
-                      <FolderCode
-                        role="button"
-                        size={20}
-                        className="text-muted-foreground cursor-pointer hover:text-primary"
-                      />
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      Existing working directories
-                    </TooltipContent>
-                  </Tooltip>
-                </PopoverTrigger>
-                <PopoverContent
-                  className="p-0 w-[580px]"
-                  side="bottom"
-                  align="end"
-                >
-                  <Command>
-                    <CommandInput placeholder="Search recent..." />
-                    <CommandList>
-                      <CommandEmpty>No results found.</CommandEmpty>
-                      <CommandGroup>
-                        {workingDirSuggestions.map((wds) => (
-                          <CommandItem
-                            key={wds.id}
-                            value={wds.workDir}
-                            onSelect={() => {
-                              field.onChange(wds.workDir);
-                              setOpen(false);
-                            }}
-                          >
-                            {wds.workDir}
-                          </CommandItem>
-                        ))}
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-            </div>
+            <Input {...field} />
           </FormControl>
           <FormMessage />
         </FormItem>
