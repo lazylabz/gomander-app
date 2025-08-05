f09e98ebf7206452db547877f08e3e256ca3ed78
moisesrj97 <moisesrj97@gmail.com>
2025-08-05 12:02:07 +0200

    refactor: extract ProjectCard

--- cmd/gomander/frontend/src/screens/ProjectSelectionScreen/ProjectSelectionScreen.tsx
+++ cmd/gomander/frontend/src/screens/ProjectSelectionScreen/ProjectSelectionScreen.tsx
@@ -1,24 +1,16 @@
-import { EllipsisVertical, Import, Plus } from "lucide-react";
+import { Import, Plus } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import { CreateProjectModal } from "@/components/modals/Project/CreateProjectModal.tsx";
 import { DeleteProjectModal } from "@/components/modals/Project/DeleteProjectModal.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu.tsx";
 import { dataService } from "@/contracts/service.ts";
 import type { Project } from "@/contracts/types.ts";
-import { fetchProject } from "@/queries/fetchProject.ts";
+import { ProjectCard } from "@/screens/ProjectSelectionScreen/components/ProjectCard.tsx";
 import { deleteProject } from "@/useCases/project/deleteProject.ts";
-import { exportProject } from "@/useCases/project/exportProject.ts";
 import { importProject } from "@/useCases/project/importProject.ts";
 
 export const ProjectSelectionScreen = () => {
-  const [projectMenuOpen, setProjectMenuOpen] = useState<string | null>(null);
   const [projectIdBeingDeleted, setProjectIdBeingDeleted] = useState<
     string | null
   >(null);
@@ -37,23 +29,6 @@ export const ProjectSelectionScreen = () => {
 
   const handleSuccess = async () => {
     await fetchAvailableProjects();
-  };
-
-  const handleOpenProject = (projectId: string) => async () => {
-    await dataService.openProject(projectId);
-    await fetchProject();
-  };
-
-  const handleSetProjectMenuOpen = (projectId: string) => (open: boolean) => {
-    if (open) {
-      setProjectMenuOpen(projectId);
-    } else {
-      setProjectMenuOpen(null);
-    }
-  };
-
-  const handleProjectMenuClick = (projectId: string) => () => {
-    setProjectMenuOpen(projectId);
   };
 
   const handleDeleteProject = (projectId: string) => async () => {
@@ -96,41 +71,10 @@ export const ProjectSelectionScreen = () => {
         {availableProjects.length > 0 && (
           <div className="flex flex-col items-center justify-center gap-2">
             {availableProjects.map((p) => (
-              <div
-                className="relative px-4 py-2 border border-neutral-100 rounded-xl shadow hover:shadow-md transition-all w-80 flex flex-col items-start gap-1"
-                key={p.id}
-              >
-                <DropdownMenu
-                  open={projectMenuOpen === p.id}
-                  onOpenChange={handleSetProjectMenuOpen(p.id)}
-                >
-                  <DropdownMenuTrigger className="absolute top-2 right-2 ">
-                    <EllipsisVertical
-                      onClick={handleProjectMenuClick(p.id)}
-                      size={16}
-                      className="text-muted-foreground cursor-pointer hover:text-primary"
-                    />
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuItem onClick={handleDeleteProject(p.id)}>
-                      Delete
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => exportProject(p.id)}>
-                      Export
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-                <p
-                  className="font-semibold hover:underline cursor-pointer"
-                  onClick={handleOpenProject(p.id)}
-                >
-                  {p.name}
-                </p>
-                <div className="flex text-sm gap-4 text-muted-foreground">
-                  <p>{Object.keys(p.commands).length} commands</p>
-                  <p>{p.commandGroups.length} command groups</p>
-                </div>
-              </div>
+              <ProjectCard
+                project={p}
+                handleDeleteProject={handleDeleteProject(p.id)}
+              />
             ))}
           </div>
         )}
---
+++ cmd/gomander/frontend/src/screens/ProjectSelectionScreen/components/ProjectCard.tsx
@@ -0,0 +1,64 @@
+import { EllipsisVertical } from "lucide-react";
+import { useState } from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu.tsx";
+import { dataService } from "@/contracts/service.ts";
+import type { Project } from "@/contracts/types.ts";
+import { fetchProject } from "@/queries/fetchProject.ts";
+import { exportProject } from "@/useCases/project/exportProject.ts";
+
+export const ProjectCard = ({
+  project,
+  handleDeleteProject,
+}: {
+  project: Project;
+  handleDeleteProject: () => void;
+}) => {
+  const [projectMenuOpen, setProjectMenuOpen] = useState<boolean>(false);
+
+  const handleOpenProject = (projectId: string) => async () => {
+    await dataService.openProject(projectId);
+    await fetchProject();
+  };
+
+  const handleProjectMenuClick = () => {
+    setProjectMenuOpen(true);
+  };
+
+  return (
+    <div className="relative px-4 py-2 border border-neutral-100 rounded-xl shadow hover:shadow-md transition-all w-80">
+      <DropdownMenu open={projectMenuOpen} onOpenChange={setProjectMenuOpen}>
+        <DropdownMenuTrigger className="absolute top-2 right-2 ">
+          <EllipsisVertical
+            onClick={handleProjectMenuClick}
+            size={16}
+            className="text-muted-foreground cursor-pointer hover:text-primary"
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={handleDeleteProject}>
+            Delete
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => exportProject(project.id)}>
+            Export
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div
+        className="flex flex-col items-start gap-1 cursor-pointer"
+        onClick={handleOpenProject(project.id)}
+      >
+        <p className="font-semibold">{project.name}</p>
+        <div className="flex text-sm gap-4 text-muted-foreground">
+          <p>{Object.keys(project.commands).length} commands</p>
+          <p>{project.commandGroups.length} command groups</p>
+        </div>
+      </div>
+    </div>
+  );
+};
