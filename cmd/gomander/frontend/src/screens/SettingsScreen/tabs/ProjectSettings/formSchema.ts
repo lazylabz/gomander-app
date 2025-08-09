7025fd0119ba0c0da233e612f1a1a21f0e4dafb9
moisesrj97 <moisesrj97@gmail.com>
2025-08-09 10:10:39 +0200

    feat: theme provider and settings

--- cmd/gomander/frontend/src/screens/SettingsScreen/tabs/ProjectSettings/formSchema.ts
+++ cmd/gomander/frontend/src/screens/SettingsScreen/tabs/ProjectSettings/formSchema.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
 
+import { availableThemes } from "@/contexts/theme.tsx";
+
 export const formSchema = z.object({
   environmentPaths: z.array(
     z.object({
       value: z.string().min(1, "Path cannot be empty"),
     }),
   ),
+  theme: z.enum(availableThemes),
 });
+
 export type FormType = z.infer<typeof formSchema>;
