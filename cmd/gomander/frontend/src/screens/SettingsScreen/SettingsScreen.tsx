2ed509c77044f25f74b9505e2196dd41c79837dc
Noel Lopez <nlopez.publi@gmail.com>
2025-08-09 17:18:38 +0200

    style(SettingsScreen): add cursor pointer to tabs

--- cmd/gomander/frontend/src/screens/SettingsScreen/SettingsScreen.tsx
+++ cmd/gomander/frontend/src/screens/SettingsScreen/SettingsScreen.tsx
@@ -37,14 +37,14 @@ export const SettingsScreen = () => {
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger
             value={SettingsTab.User}
-            className="flex items-center space-x-2"
+            className="flex items-center space-x-2 cursor-pointer"
           >
             <User className="h-4 w-4" />
             <span>User Settings</span>
           </TabsTrigger>
           <TabsTrigger
             value={SettingsTab.Project}
-            className="flex items-center space-x-2"
+            className="flex items-center space-x-2 cursor-pointer"
           >
             <PanelsTopLeft className="h-4 w-4" />
             <span>Project Settings</span>
