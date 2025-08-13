496bffad87bdddcc1532bdd19de63c66990ae8ae
Noel Lopez <nlopez.publi@gmail.com>
2025-08-13 07:56:49 +0200

    feat: optimize Filter func slice initialization

--- internal/helpers/array/array.go
+++ internal/helpers/array/array.go
@@ -1,7 +1,7 @@
 package array
 
 func Filter[T any](array []T, predicate func(T) bool) []T {
-	result := make([]T, 0)
+	result := make([]T, 0, len(array))
 	for _, item := range array {
 		if predicate(item) {
 			result = append(result, item)
