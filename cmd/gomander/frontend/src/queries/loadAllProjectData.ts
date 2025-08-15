8fcf12c0b003af9b5840f8a1619e5055b1d8ac21
Noel Lopez <nlopez.publi@gmail.com>
2025-08-15 13:43:33 +0200

    feat(frontend): adapt initial load

--- cmd/gomander/frontend/src/queries/loadAllProjectData.ts
+++ cmd/gomander/frontend/src/queries/loadAllProjectData.ts
@@ -1,26 +1,7 @@
-import { dataService } from "@/contracts/service.ts";
-import { commandGroupStore } from "@/store/commandGroupStore.ts";
-import { commandStore } from "@/store/commandStore.ts";
-import { projectStore } from "@/store/projectStore.ts";
+import { fetchCommandGroups } from "@/queries/fetchCommandGroups.ts";
+import { fetchCommands } from "@/queries/fetchCommands.ts";
+import { fetchProject } from "@/queries/fetchProject.ts";
 
 export const loadAllProjectData = async () => {
-  const { setCommands } = commandStore.getState();
-  const { setCommandGroups } = commandGroupStore.getState();
-  const { setProjectInfo } = projectStore.getState();
-
-  const [commands, commandGroups, projectInfo] = await Promise.all([
-    dataService.getCommands(),
-    dataService.getCommandGroups(),
-    dataService.getCurrentProject(),
-  ]);
-
-  console.log({
-    commands,
-    commandGroups,
-    projectInfo,
-  });
-
-  setCommands(commands);
-  setCommandGroups(commandGroups);
-  setProjectInfo(projectInfo);
+  await Promise.all([fetchProject(), fetchCommands(), fetchCommandGroups()]);
 };
