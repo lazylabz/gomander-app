1d234dbeeb6c528472998637167939d07c3bdff8
moisesrj97 <moisesrj97@gmail.com>
2025-08-16 12:33:43 +0200

    test: add tests for helpers

--- internal/project/domain/project.go
+++ internal/project/domain/project.go
@@ -5,12 +5,3 @@ type Project struct {
 	Name             string `json:"name"`
 	WorkingDirectory string `json:"workingDirectory"`
 }
-
-func (p *Project) Equals(other *Project) bool {
-	if p == nil || other == nil {
-		return p == other
-	}
-	return p.Id == other.Id &&
-		p.Name == other.Name &&
-		p.WorkingDirectory == other.WorkingDirectory
-}
