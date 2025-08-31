331fff720f67c292a3caaa4e0bb8599959cea107
Noel Lopez <nlopez.publi@gmail.com>
2025-08-31 14:15:18 +0200

    test: add tests for NewCommandDuplicatedEvent function

--- internal/command/domain/event/commandduplicated_test.go
+++ internal/command/domain/event/commandduplicated_test.go
@@ -12,3 +12,12 @@ func TestCommandDuplicatedEvent_GetName(t *testing.T) {
 	e := event.CommandDuplicatedEvent{}
 	assert.Equal(t, "domain_event.command.duplicate", e.GetName())
 }
+
+func TestNewCommandDuplicatedEvent(t *testing.T) {
+	commandId := "1234"
+	insideGroupId := "5678"
+	e := event.NewCommandDuplicatedEvent(commandId, insideGroupId)
+	assert.NotNil(t, e)
+	assert.Equal(t, commandId, e.CommandId)
+	assert.Equal(t, insideGroupId, e.InsideGroupId)
+}
