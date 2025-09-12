0f8aa07486e2005624a0a9d0e446061d989d3a7b
moisesrj97 <moisesrj97@gmail.com>
2025-09-12 08:27:28 +0200

    feat: implement FSInput

---
+++ cmd/gomander/frontend/src/components/inputs/FSInput.tsx
@@ -0,0 +1,29 @@
+import { FolderIcon } from "lucide-react";
+import type { ChangeEvent } from "react";
+
+import { Input } from "@/components/ui/input.tsx";
+import { helpersService } from "@/contracts/service.ts";
+
+export const FSInput = (props: React.ComponentProps<typeof Input>) => {
+  const handleExploreFs = async () => {
+    const path = await helpersService.getFolderPath();
+    if (path) {
+      props.onChange?.({
+        target: { value: path },
+      } as ChangeEvent<HTMLInputElement>);
+    }
+  };
+
+  return (
+    <div className="w-full relative">
+      <Input {...props} />
+      <div className="absolute right-2 top-2 z-10 bg-background">
+        <FolderIcon
+          onClick={handleExploreFs}
+          size={20}
+          className="text-foreground opacity-75 hover:opacity-100 transition-opacity cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+};
