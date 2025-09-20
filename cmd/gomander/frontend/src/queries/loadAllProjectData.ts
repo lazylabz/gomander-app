8ae4155a56c5a94f516b6fca39bebadc85f612fd
Noel Lopez <nlopez.publi@gmail.com>
2025-09-20 21:52:15 +0200

    chore(i18n): add temporary testing code for i18n PoC

--- cmd/gomander/frontend/src/queries/loadAllProjectData.ts
+++ cmd/gomander/frontend/src/queries/loadAllProjectData.ts
@@ -1,3 +1,4 @@
+import { translationsService } from "@/contracts/service";
 import { fetchCommandGroups } from "@/queries/fetchCommandGroups.ts";
 import { fetchCommands } from "@/queries/fetchCommands.ts";
 import { fetchProject } from "@/queries/fetchProject.ts";
@@ -5,4 +6,12 @@ import { fetchProject } from "@/queries/fetchProject.ts";
 export const loadAllProjectData = async () => {
   await Promise.all([fetchCommands(), fetchCommandGroups()]);
   await fetchProject();
+
+  // TODO: Delete this (testing i18n PoC)
+  const languages = await translationsService.getSupportedLanguages();
+  console.log("languages: ", languages);
+
+  const translations = await translationsService.getTranslation('es-ES');
+  console.log("translations: ", translations)
+  console.log("sidebar title: ", translations["sidebar.title"])
 };
