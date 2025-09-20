c07dce712b64749eef58968b8350bf7256f409c3
Noel Lopez <nlopez.publi@gmail.com>
2025-09-20 21:49:56 +0200

    feat(config): add locale field to Config domain

--- internal/config/domain/config.go
+++ internal/config/domain/config.go
@@ -9,4 +9,5 @@ type Config struct {
 	LastOpenedProjectId string            `json:"lastOpenedProjectId"`
 	EnvironmentPaths    []EnvironmentPath `json:"environmentPaths"`
 	LogLineLimit        int               `json:"logLineLimit"`
+	Locale              string            `json:"locale"`
 }
