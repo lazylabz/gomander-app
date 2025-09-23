b4ac8ffcafad0793d9f7560c02014281b75acc0d
SujayCH1 <suju.narayan@gmail.com>
2025-09-23 15:41:44 +0000

    updated models in backed to accept the failurePattern field - 2

--- internal/project/domain/project.go
+++ internal/project/domain/project.go
@@ -4,4 +4,5 @@ type Project struct {
 	Id               string `json:"id"`
 	Name             string `json:"name"`
 	WorkingDirectory string `json:"workingDirectory"`
+	FailurePatterns  []string `json:"failurePatterns,omitempty"`
 }
