9985d2bee7f94c8e71d6ca7567312f76f54a3e96
SujayCH1 <suju.narayan@gmail.com>
2025-09-23 15:41:20 +0000

    updated models in backed to accept the failurePattern filed

--- cmd/gomander/frontend/wailsjs/go/models.ts
+++ cmd/gomander/frontend/wailsjs/go/models.ts
@@ -97,7 +97,7 @@ export namespace domain {
 	    id: string;
 	    name: string;
 	    workingDirectory: string;
-		failurePatterns?: string[];
+	    failurePatterns?: string[];
 	}
 	export interface ProjectExportJSONv1 {
 	    version: number;
