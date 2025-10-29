3c63462922c585906fb7c489e44a5ab278acd154
moisesrj97 <moisesrj97@gmail.com>
2025-10-29 16:57:23 +0100

    feat(frontend): ensure error status is cleared on command (or group) rerun

--- cmd/gomander/frontend/src/useCases/commandGroup/runCommandGroup.ts
+++ cmd/gomander/frontend/src/useCases/commandGroup/runCommandGroup.ts
@@ -1,5 +1,8 @@
 import { dataService } from "@/contracts/service.ts";
+import { cleanCommandGroupFromErrors } from "@/useCases/command/cleanCommandGroupFromErrors.ts";
 
 export const runCommandGroup = async (groupId: string) => {
+  cleanCommandGroupFromErrors(groupId);
+
   await dataService.runCommandGroup(groupId);
 };
